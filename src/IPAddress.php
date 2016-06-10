cdcbcc756355a4f0672a4a247353f77c5b57afbe
Michael Hall <hall@michaelhall.se>
2016-06-10 22:54:28 +0200

    Add IP address validation

--- src/IPAddress.php
+++ src/IPAddress.php
@@ -2,6 +2,7 @@
 
 namespace DataTypes;
 
+use DataTypes\Exceptions\IPAddressInvalidArgumentException;
 use DataTypes\Interfaces\IPAddressInterface;
 
 /**
@@ -18,7 +19,11 @@ class IPAddress implements IPAddressInterface
     {
         assert(is_string($ipAddress), '$ipAddress is not a string');
 
-        $this->_value = $ipAddress;
+        if (!static::_parse($ipAddress, false, $octets, $error)) {
+            throw new IPAddressInvalidArgumentException($error);
+        }
+
+        $this->_octets = $octets;
     }
 
     /**
@@ -26,11 +31,113 @@ class IPAddress implements IPAddressInterface
      */
     public function __toString()
     {
-        return $this->_value;
+        return implode('.', $this->_octets);
     }
 
     /**
-     * @var string My value.
+     * Tries to parse an IP address and returns the result or error text.
+     *
+     * @param string      $ipAddress    The IP address.
+     * @param bool        $validateOnly If true only validation is performed, if false parse results are returned.
+     * @param int[]|null  $octets       The octets if parsing was successful, undefined otherwise.
+     * @param string|null $error        The error text if parsing was not successful, undefined otherwise.
+     *
+     * @return bool True if parsing was successful, false otherwise.
      */
-    private $_value;
+    private static function _parse($ipAddress, $validateOnly, array &$octets = null, &$error = null)
+    {
+        // Pre-validate IP address.
+        if (!static::_preValidate($ipAddress, $error)) {
+            return false;
+        }
+
+        // Split IP address in parts.
+        $ipAddressParts = explode('.', $ipAddress);
+
+        // IP address must have four parts.
+        if (count($ipAddressParts) !== 4) {
+            $error = 'IP address "' . $ipAddress . '" is invalid: IP address must consist of four octets.';
+
+            return false;
+        }
+
+        // Validate the parts.
+        $octets = [];
+        foreach ($ipAddressParts as $ipAddressPart) {
+            if (!static::_validateIpAddressPart($ipAddressPart, $octet, $error)) {
+                $error = 'IP address "' . $ipAddress . '" is invalid: ' . $error;
+
+                return false;
+            }
+
+            // Save the resulting octet.
+            if (!$validateOnly) {
+                $octets[] = $octet;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Pre-validates a IP address.
+     *
+     * @param string $ipAddress The IP address.
+     * @param string $error     The error text if pre-validation was not successful, undefined otherwise.
+     *
+     * @return bool True if pre-validation was successful, false otherwise.
+     */
+    private static function _preValidate($ipAddress, &$error)
+    {
+        // Empty IP address is invalid.
+        if ($ipAddress === '') {
+            $error = 'IP address "' . $ipAddress . '" is empty.';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates an IP address part
+     *
+     * @param string $ipAddressPart The IP address part.
+     * @param int    $octet         The resulting octet.
+     * @param string $error         The error text if validation was not successful, undefined otherwise.
+     *
+     * @return bool True if validation was successful, false otherwise.
+     */
+    private static function _validateIpAddressPart($ipAddressPart, &$octet, &$error)
+    {
+        // Empty octet is invalid.
+        if ($ipAddressPart === '') {
+            $error = 'Octet "' . $ipAddressPart . '" is empty.';
+
+            return false;
+        }
+
+        // Octet containing invalid character is invalid.
+        if (preg_match('/[^0-9]/', $ipAddressPart, $matches)) {
+            $error = 'Octet "' . $ipAddressPart . '" contains invalid character "' . $matches[0] . '".';
+
+            return false;
+        }
+
+        $octet = intval($ipAddressPart);
+
+        // Octet out of range is invalid.
+        if ($octet > 255) {
+            $error = 'Octet "' . $ipAddressPart . '" is out of range: Maximum value for octet is 255.';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @var int[] My octets.
+     */
+    private $_octets;
 }
--- tests/IPAddressTest.php
+++ tests/IPAddressTest.php
@@ -15,4 +15,56 @@ class IPAddressTest extends PHPUnit_Framework_TestCase
         $this->assertSame('127.0.0.1', (new IPAddress('127.0.0.1'))->__toString());
         $this->assertSame('255.255.255.255', (new IPAddress('255.255.255.255'))->__toString());
     }
+
+    /**
+     * Test that empty IP address is invalid.
+     *
+     * @expectedException DataTypes\Exceptions\IPAddressInvalidArgumentException
+     * @expectedExceptionMessage IP address "" is empty.
+     */
+    public function testEmptyIPAddressIsInvalid()
+    {
+        new IPAddress('');
+    }
+
+    /**
+     * Test that empty IP address with not four octets is invalid.
+     *
+     * @expectedException DataTypes\Exceptions\IPAddressInvalidArgumentException
+     * @expectedExceptionMessage IP address "1.2.3" is invalid: IP address must consist of four octets.
+     */
+    public function testIPAddressWithNotFourOctetsIsInvalid()
+    {
+        new IPAddress('1.2.3');
+    }
+
+    /**
+     * Test that IP address with empty octet is invalid.
+     * @expectedException DataTypes\Exceptions\IPAddressInvalidArgumentException
+     * @expectedExceptionMessage IP address "192.168..1" is invalid: Octet "" is empty.
+     */
+    public function testIPAddressWithEmptyOctetIsInvalid()
+    {
+        new IPAddress('192.168..1');
+    }
+
+    /**
+     * Test that IP address with invalid character in octet is invalid.
+     * @expectedException DataTypes\Exceptions\IPAddressInvalidArgumentException
+     * @expectedExceptionMessage IP address "127.0.0X.1" is invalid: Octet "0X" contains invalid character "X".
+     */
+    public function testIPAddressWithInvalidCharacterInOctetIsInvalid()
+    {
+        new IPAddress('127.0.0X.1');
+    }
+
+    /**
+     * Test that IP address with octet out of range is invalid.
+     * @expectedException DataTypes\Exceptions\IPAddressInvalidArgumentException
+     * @expectedExceptionMessage IP address "255.255.256.255" is invalid: Octet "256" is out of range: Maximum value for octet is 255.
+     */
+    public function testIPAddressWithOctetOutOfRangeIsInvalid()
+    {
+        new IPAddress('255.255.256.255');
+    }
 }
