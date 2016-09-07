1744a8e14260da44e821b32a22808b403c4f5ada
Michael Hall <hall@michaelhall.se>
2016-09-07 17:27:48 +0200

    Add some methods to FilePathInterface

--- src/Interfaces/FilePathInterface.php
+++ src/Interfaces/FilePathInterface.php
@@ -7,4 +7,23 @@ namespace DataTypes\Interfaces;
  */
 interface FilePathInterface extends DataTypeInterface
 {
+    /**
+     * @return string[] The directory parts.
+     */
+    public function getDirectoryParts();
+
+    /**
+     * @return string|null The filename or null if the file path is a directory.
+     */
+    public function getFilename();
+
+    /**
+     * @return bool True if file path is absolute, false otherwise.
+     */
+    public function isAbsolute();
+
+    /**
+     * @return bool True if file path is relative, false otherwise.
+     */
+    public function isRelative();
 }
