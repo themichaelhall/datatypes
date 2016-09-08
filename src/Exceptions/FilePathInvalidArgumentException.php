e8fb55e8e255ce3a957934084beebf4d0990bbda
Michael Hall <hall@michaelhall.se>
2016-09-08 18:43:36 +0200

    Add FilePathInvalidArgumentException class

---
+++ src/Exceptions/FilePathInvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DataTypes\Exceptions;
+
+/**
+ * Exception used when passed invalid argument in FilePath class.
+ */
+class FilePathInvalidArgumentException extends \InvalidArgumentException
+{
+}
---
+++ tests/Exceptions/FilePathInvalidArgumentExceptionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use DataTypes\Exceptions\FilePathInvalidArgumentException;
+
+/**
+ * Test FilePathInvalidArgumentException class.
+ */
+class FilePathInvalidArgumentExceptionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that FilePathInvalidArgumentException is subclass of InvalidArgumentException.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage This is a FilePathInvalidArgumentException.
+     */
+    public function testFilePathInvalidArgumentExceptionIsInvalidArgumentException()
+    {
+        throw new FilePathInvalidArgumentException('This is a FilePathInvalidArgumentException.');
+    }
+}
